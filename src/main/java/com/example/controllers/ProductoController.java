9b83f8073dcd977f7d6914c7975a40928ec3cc00
rubenkiworks <rubengomezkiworks@gmail.com>
2024-11-18 19:18:46 +0100

    validacion terminada

--- src/main/java/com/example/controllers/ProductoController.java
+++ src/main/java/com/example/controllers/ProductoController.java
@@ -1,6 +1,9 @@
 package com.example.controllers;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -8,7 +11,11 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +23,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.entities.Producto;
 import com.example.services.ProductoService;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 
@@ -69,5 +77,26 @@ public class ProductoController {
 
     }
 
+    @PostMapping
+    public ResponseEntity<Map<String, Object>> saveProducto(@Valid @RequestBody Producto producto, BindingResult results){
+        
+        ResponseEntity<Map<String, Object>> responseEntity = null;
+        Map<String, Object> responseAsMap = new HashMap<>();
+
+        if (results.hasErrors()) {
+            List<String> mensajesError = new ArrayList<>();
+
+            List<ObjectError> objectErrors =  results.getAllErrors();
+
+            objectErrors.stream().forEach(o -> mensajesError.add(o.getDefaultMessage()));
+
+            responseAsMap.put("errores", mensajesError);
+            responseAsMap.put("producto", producto);
+            
+            responseEntity = new ResponseEntity<>(responseAsMap, HttpStatus.BAD_REQUEST);
+        }
+
+        return responseEntity;
+    }
 
 }
--- src/main/java/com/example/entities/Producto.java
+++ src/main/java/com/example/entities/Producto.java
@@ -11,6 +11,11 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -29,11 +34,22 @@ public class Producto implements Serializable{
     @Id
     @GeneratedValue(strategy=GenerationType.IDENTITY)
     private int id;
+    
+    @NotNull(message="El nombre del producto no puede ser nulo")
+    @NotEmpty(message="El nombre del producto no puede estar vacio")
+    @Size(min=4, max=25, message="El nombre del producto tiene que estar entre 4 y 25 caracteres")
     private String name;
+
+    @NotBlank(message="La descripcion es requerida")
     private String description;
+
+    @Min(value=0, message="El stock no puede ser negativo")
     private int stock;
+
+    @Min(value=0, message="El precio no puede ser negativo")
     private double price;
 
+    @NotNull(message="La presentacion es requerida")
     @ManyToOne(fetch=FetchType.LAZY)
     @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
     private Presentacion presentacion;
